bc0d8e8d2a37195fa66417e443aa9a9a09c38c73
mahdiulchowdhury <mahdiulc@bu.edu>
2019-05-12 19:53:20 -0400

    ok

--- unique_character.cpp
+++ unique_character.cpp
@@ -2,18 +2,20 @@
 #include <string>
 
 using namespace std; 
+bool isUniqueChars(string cha);
 
 
 int main() 
 {
 	//string cha = "abcdd";
-	string cha = "abc";
+	string cha = "abcdd";
 	int index = 0; 
 	int index_two = 0;
-	int a = 0; 
+	//int a = 0; 
+	bool a = true; 
 	
 	//cout << "lenght of string:" << cha.length() <<endl; 
-	for (index; index<cha.length()-1; index++)
+	/*for (index; index<cha.length()-1; index++)
 	{
 		for (index_two = index+1;index_two<cha.length(); index_two++)
 		{
@@ -32,8 +34,34 @@ int main()
 		}
 		
 	}
-	cout<< "unique string"<<endl; 
+	cout<< "unique string"<<endl; */
 	
+	//method 2
+	a = isUniqueChars(cha);
+	if (a == false)
+	{
+		cout<<"Not a unique string"<<endl;
+	}
+	else 
+		cout<<"unique string"<<endl; 
+
 
 	return 0; 
+}
+
+bool isUniqueChars(string cha)
+{
+	if(cha.length() > 128) //there are 128 unique chars 
+		return false; 
+	bool *char_set = new bool [128];
+	for (int i =0; i<cha.length(); i++)
+	{
+		int val = cha.at(i); 
+		if (char_set[val]) //already found not unique 
+		{
+			return false;
+		}
+		char_set [val] = true; 
+	}
+	return true; 
 }
