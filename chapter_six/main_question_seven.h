a352d489c272580046669e9fe813e65e6bc67429
mchowdhury-tft <mahdiul.chowdhury@topflighttech.com>
2019-10-13 22:19:03 -0400

    Question seven

---
+++ chapter_six/main_question_seven.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_QUESTION_SEVEN 
+#define MAIN_QUESTION_SEVEN
+
+
+class Angle{
+private: 
+	int degrees; 
+	float minutes; 
+	char direction; 
+public: 
+	void user_input(); 
+	void display(); 
+	
+
+}
+
+
+
+
+#endif 
