98e263fd40294af1b3efdf6f296c95ee091b0cf2
mahdiulchowdhury <mahdiulc@bu.edu>
2020-04-16 20:23:56 -0400

    embedded c

---
+++ Embedded-C/My_workspace/hosts/000Test/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 2, 2020
+ *      Author: optimusprime
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello world\n");
+	printf("Press 'Enter' to exit this application\n");
+	getchar();
+	return 0;
+}
